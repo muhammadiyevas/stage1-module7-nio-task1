4c50f05623deb31389400e57e92588f53fcb796b
MuhammadiyevaSevinch <muhammadiyeva133@gmail.com>
2023-07-04 21:34:01 +0500

    nio-task7

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,31 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        Path path = Path.of(file.getPath());
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(line -> {
+                if (line.startsWith("Name: ")) {
+                    profile.setName(line.substring(6));
+                } else if (line.startsWith("Age: ")) {
+                    profile.setAge(Integer.parseInt(line.substring(5)));
+                } else if (line.startsWith("Email: ")) {
+                    profile.setEmail(line.substring(7));
+                } else if (line.startsWith("Phone: ")) {
+                    profile.setPhone(Long.parseLong(line.substring(7)));
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
